9c71e82df3821c59c678c8b200894048e3451392
a-elnajjar <code@mail.com>
2024-05-19 21:54:54 -0400

    Initial Commit

---
+++ VideoPlayerStream/VideoPlayerStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerStreamApp.swift
+//  VideoPlayerStream
+//
+//  Created by Abdalla Elnajjar on 2024-05-19.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
